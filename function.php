b95938e9caa3d1d6d389f3d7449e2ce26916d5b4
Alhillawii <ayahhillawi9@gmail.com>
2024-07-11 15:14:39 +0300

    message

--- function.php
+++ function.php
@@ -69,23 +69,6 @@
 
      //5//????????
      echo '<hr>';
-    //  $num=407;  
-    //  $total=0;  
-    //  $x=$num;  
-    //  while($x!=0)  
-    //  {  
-    //  $rem=$x%10;  
-    //  $total=$total+$rem*$rem*$rem;  
-    //  $x=$x/10;  
-    //  }  
-    //  if($num==$total)  
-    //  {  
-    //  echo "407 is Armstrong Number ";  
-    //  }  
-    //  else  
-    //  {  
-    //  echo "407 is not Armstrong Number ";  
-    //  }  
      function Armstrong($num)  {
         $total = 0;
         $x = $num;
--- stringtas.php
+++ stringtas.php
@@ -1,166 +1,148 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Document</title>
 </head>
+
 <body>
-    <?php
-    ///1/a///
-    $word = "this Tasks It's So Bigger Coach Moumen";
-    $uppercase = strtoupper($word);
-    echo $uppercase;
-     ///1/b///
-     echo '<hr>';
-     $lower = strtolower($word);
-     echo $lower;
-     //1/c//
-     echo '<hr>';
-     $firstltter = ucfirst($word);
-     echo $firstltter;
-     //1/d//
-     echo '<hr>';
-     $eachword = ucwords( "moumen" ,$word );
-     echo $eachword;
+   <?php
+   ///1/a///
+   $word = "this Tasks It's So Bigger Coach Moumen";
+   $uppercase = strtoupper($word);
+   echo $uppercase;
+   ///1/b///
+   echo '<hr>';
+   $lower = strtolower($word);
+   echo $lower;
+   //1/c//
+   echo '<hr>';
+   $firstltter = ucfirst($word);
+   echo $firstltter;
+   //1/d//
+   echo '<hr>';
+   $eachword = ucwords("moumen", $word);
+   echo $eachword;
 
-     ///2///
-     echo '<hr>';
-     $time = "085119";
-     $hour = substr($time , 0 ,2);
-     $minutes = substr($time , 2 ,2);
-     $seconds = substr($time , 4 , 2);
-     $date = $hour . ":" . $minutes . ":" . $seconds;
-     echo $date;
+   ///2///
+   echo '<hr>';
+   $time = "085119";
+   $hour = substr($time, 0, 2);
+   $minutes = substr($time, 2, 2);
+   $seconds = substr($time, 4, 2);
+   $date = $hour . ":" . $minutes . ":" . $seconds;
+   echo $date;
 
-     //3//
-     echo '<hr>';
-     $sentence = "I am a full stack developer at orange coding academy";
-     $put = "orange";
-     if (stripos($sentence , $put) !== false){
-        echo "word Found \n";
-     }else {
-        echo "word not  found";
-     }
+   //3//
+   echo '<hr>';
+   $sentence = "I am a full stack developer at orange coding academy";
+   $put = "orange";
+   if (stripos($sentence, $put) !== false) {
+      echo "word Found \n";
+   } else {
+      echo "word not  found";
+   }
 
-     //4//
-     echo '<hr>';
-     $url = "www.orange.com/index.php";
-     $file_name = basename($url);
-     echo $file_name;
+   //4//
+   echo '<hr>';
+   $url = "www.orange.com/index.php";
+   $file_name = basename($url);
+   echo $file_name;
 
-     //5//
-     echo '<hr>';
-     $email =  "info@orange.com";
-     $username = strtr($email , '@' , true);
-     echo $username;
+   //5//
+   echo '<hr>';
+   $email =  "info@orange.com";
+   $username = strtr($email, '@', true);
+   echo $username;
 
-     //6//
-     echo '<hr>';
-     $string = "info@orange.com";
-     $lastthree = substr($string , -3);
-     echo $lastthree;
+   //6//
+   echo '<hr>';
+   $pas = "1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcefghijklmnopqrstuvwxyz";
+   $paslen = 10;
+   echo substr(str_shuffle($pas), 0, $paslen);
 
-     //7//?????
-     echo '<hr>';
-       //  function get($strings , $lenght){
-    //     $m = strlen($strings) - 1;
-    //     $pasword = '';
+   //7/
+   echo '<hr>';
+   echo substr($username, -3);
 
-    //     for ($i = 0 ; $i < $lenght; $i++){
-    //         $index = random_int(0 , $m);
-    //         $pasword .= $strings[$index];
-    //     }
-    //     return $pasword;
-        
-    //  }
-    //  $strings = '1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-    //  $pasword_lenght = 8;
-    //  $ran = get($strings , $pasword_lenght);
-    //  echo $ran;
-     //??????????//
+   //8//?????
+   echo '<hr>';
+   //   echo str_replace("That new trainee is so genius." , "Our" , $stri);
 
-     //8//?????
-     echo '<hr>';
-   //   function replaceFirstWord($sentence, $new_word) {
-   //      $words = explode(' ', $sentence);
-   //      $words[0] = $new_word;
-   //      $new_sentence = implode(' ', $words);
-        
-   //      return $new_sentence;
-   //  }
-   //  $sentence = 'That new trainee is so genius.';
-   //  $new_word = 'Our';
-   //  $new_sentence = replaceFirstWord($sentence, $new_word);
-   //  echo  $new_sentence;
 
-    //9//
-    echo ' <hr>';
-    $twin = "Twinkle, twinkle, little star.";
-    $array = explode(" ", $twin);
-    var_dump($array);
+   //9//
+   echo ' <hr>';
+   $twin = "Twinkle, twinkle, little star.";
+   $array = explode(" ", $twin);
+   var_dump($array);
 
-    ///10//
-    echo '<hr>';
-    echo strcmp('dragonball','dragonball');
+   //     ///10//
+   echo '<hr>';
+   $Character = "z";
+   if ($Character == 'z') {
+      echo "a";
+   } else {
+      echo chr(ord($Character) + 1);
+   }
 
-    //11/????
-    echo '<hr>';
-//     function getNextLetter($letter) {
-//       if (strlen($letter) !== 1 || !ctype_alpha($letter)) {
-//           return "Invalid input. Please enter a single alphabetical character.";
-//       }
-//       $letter = strtolower($letter);
-//       $nextLetter = chr((ord($letter) - ord('a') + 1) % 26 + ord('a'));
-  
-//       return $nextLetter;
-//   }
+   //     //11/??
+   echo '<hr>';
+   $word1 = "dragonball";
+   $word2 = "dragonboll";
 
-//   $inputLetter = 'a';
-// echo "Input Letter: $inputLetter\n";
-// $nextLetter = getNextLetter($inputLetter);
-// echo "Next Letter: $nextLetter\n";
+   for ($i = 0; $i < strlen($word1); $i++) {
+      echo "First difference between two strings at position " . $i . " : " . $word1[$i] . " vs " . $word2[$i];
+      break;
+   }
 
-    //12//
-    echo '<hr>';
-    $st = 'The brown fox';
-    $insert = ' quik';
-    $insert2 = 4;
-    $newstr = substr_replace($st, $insert, $insert2, 0);
-    echo $newstr;
 
-    //13//
-    echo '<hr>';
-    $num = "000065722.24";
-    $tri =  trim($num , 0);
-    echo $tri;
+   //     //12//
+   echo '<hr>';
+   $st = 'The brown fox';
+   $insert = ' quik';
+   $insert2 = 4;
+   $newstr = substr_replace($st, $insert, $insert2, 0);
+   echo $newstr;
 
-    //14//
-    echo '<hr>';
-    $text = 'The quick brwon fox jumps over the lazy dog';
-    $te = 'fox';
-    $replace =  str_replace($text , "" , $te);
-    echo $replace;
+   //     //13//
+   echo '<hr>';
+   $num = "000065722.24";
+   $tri =  trim($num, 0);
+   echo $tri;
 
-    //15//
-    echo  '<hr>';
-    $textt = "The quick brown fox jumps over the lazy dog---";
-    $tes = trim($textt , "-");
-    echo $tes;
+   //     //14//
+   echo '<hr>';
+   $text = 'The quick brwon fox jumps over the lazy dog';
+   $te = 'fox';
+   $replace =  str_replace($text, "", $te);
+   echo $replace;
 
-    //16//
-    echo '<hr>';
-    $Special = "2,543.12";
-    $nums = str_replace("," , "" , $Special);
-    echo $nums;
+   //     //15//
+   echo  '<hr>';
+   $textt = "The quick brown fox jumps over the lazy dog---";
+   $tes = trim($textt, "-");
+   echo $tes;
 
-    //17//
-    echo '<hr>';
-    $wwrod = "The quick brown fox jumps over the lazy dog";
-    $testtt = implode(" ", array_slice(explode(" ", $wwrod), 0, 5));
-    echo $testtt;
+   //     //16//
+   echo '<hr>';
 
-    //18//
+   $Special = '\"\1+2/3*2:2-3/4*3';
+   echo  str_replace(array('\"', '"', '/', '-', '+', ':', '*', '\\'), ' ', $Special);
+   echo "<hr>";
+
+   //     //17//
+   echo '<hr>';
+   $wwrod = "The quick brown fox jumps over the lazy dog";
+   $testtt = implode(" ", array_slice(explode(" ", $wwrod), 0, 5));
+   echo $testtt;
+
+   //     //18//
+   echo '<hr>';
+   $numst = "2,543.12";
+   echo str_replace(",", "", $numst);
+   echo "<hr>";
 
 
 
@@ -168,8 +150,9 @@
 
 
 
-?>   
+   ?>
 
 
 </body>
+
 </html>
